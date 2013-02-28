d2c419a3b59f003840f462126537e26f18821ef2
Ryan Stradling <ryanstradling@gmail.com>
2013-02-28 11:15:54 -0500

    Version alpha.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.strad/datomic-crate "0.8.0"
+(defproject org.clojars.strad/datomic-crate "0.8.0-alpha.1"
   :description "A datomic crate for pallet"
   :url "http://github.com/rstradling/datomic-crate"
   :license {:name "Eclipse Public License"
