262e34dbfee19802104cfa905f55446e72931fc0
Ryan Stradling <ryanstradling@gmail.com>
2013-03-04 14:23:16 -0500

    Upgraded to an actual version of pallet rather than a snapshot and changed the version number of this create to 0.8.0 as it has been tested

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject org.clojars.strad/datomic-crate "0.8.0-SNAPSHOT"
+(defproject org.clojars.strad/datomic-crate "0.8.0"
   :description "A datomic crate for pallet"
   :url "http://github.com/rstradling/datomic-crate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.palletops/pallet "0.8.0-SNAPSHOT"]])
+                 [com.palletops/pallet "0.8.0-beta.3"]])
