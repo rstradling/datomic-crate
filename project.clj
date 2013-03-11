e55e985cd0b86af6dddcb0c28ec04866011d86af
Ryan Stradling <ryanstradling@gmail.com>
2013-03-11 09:42:23 -0400

    Updating to 0.8.2-SNAPSHOT for a future release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.strad/datomic-crate "0.8.1"
+(defproject org.clojars.strad/datomic-crate "0.8.2-SNAPSHOT"
   :description "A datomic crate for pallet"
   :url "http://github.com/rstradling/datomic-crate"
   :license {:name "Eclipse Public License"
