b3aae6816b5cb5f2e5cfa7ea51baf6903e1f22c0
Ryan Stradling <ryanstradling@gmail.com>
2013-04-23 16:12:58 -0400

    Added a profiles file so the tests will work

---
+++ profiles.clj
@@ -0,0 +1,6 @@
+{:dev {:dependencies
+       [[com.palletops/crates "0.1.0"]
+        [com.palletops/pallet "0.8.0-beta.8" :classifier "tests"]
+        [ch.qos.logback/logback-classic "1.0.9"]]
+       :plugins [[codox/codox.leiningen "0.6.4"]
+                 [lein-marginalia "0.7.1"]]}}
